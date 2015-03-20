00a408f0283e57339bec9ecd932797e77c3e1f8d
Eduardo MARIN IZQUIERDO <eduardo.marin.contractor@dev.bbva.com>
2015-03-20 10:53:53 +0100

    first commit

---
+++ js/background.js
@@ -0,0 +1,20 @@
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create('../main.html', {
+    id: "GDriveExample",
+    innerBounds: {
+      width: 1100,
+      height: 800,
+      minWidth: 1100,
+      minHeight: 800
+    },
+    frame: 'none'
+  });
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('installed');
+});
+
+chrome.runtime.onSuspend.addListener(function() { 
+  // Do some simple clean-up tasks.
+});
