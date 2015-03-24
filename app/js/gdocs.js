c753c37286fe410d197fa00c8fec3f52b98f4830
Eduardo MARIN IZQUIERDO <eduardo.marin.contractor@dev.bbva.com>
2015-03-24 17:26:45 +0100

    updated

--- app/js/gdocs.js
+++ app/js/gdocs.js
@@ -2,131 +2,27 @@
 
 function GDocs(selector) {
 
-    /**
-     * Options for the Realtime loader.
-     */
-    var realtimeOptions = {
-        /**
-         * Client ID from the console.
-         */
-        clientId: '467472285488-lo80kj4n1tkr0qhe08rajn4ti827ma4q.apps.googleusercontent.com',
+  var SCOPE_ = 'https://www.googleapis.com/drive/v2/';
 
-        /**
-         * The ID of the button to click to authorize. Must be a DOM element ID.
-         */
-        authButtonElementId: 'authorizeButton',
+  this.lastResponse = null;
 
-        /**
-         * Function to be called when a Realtime model is first created.
-         */
-        initializeModel: initializeModel,
+  this.__defineGetter__('SCOPE', function() {
+    return SCOPE_;
+  });
 
-        /**
-         * Autocreate files right after auth automatically.
-         */
-        autoCreate: true,
+  this.__defineGetter__('DOCLIST_FEED', function() {
+    return SCOPE_ + 'files';
+  });
 
-        /**
-         * The name of newly created Drive files.
-         */
-        defaultTitle: "New Realtime Quickstart File",
+  this.__defineGetter__('CREATE_SESSION_URI', function() {
+    return 'https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable';
+  });
 
-        /**
-         * The MIME type of newly created Drive Files. By default the application
-         * specific MIME type will be used:
-         *     application/vnd.google-apps.drive-sdk.
-         */
-        newFileMimeType: null, // Using default.
-
-        /**
-         * Function to be called every time a Realtime file is loaded.
-         */
-        onFileLoaded: onFileLoaded,
-
-        /**
-         * Function to be called to inityalize custom Collaborative Objects types.
-         */
-        registerTypes: null, // No action.
-
-        /**
-         * Function to be called after authorization and before loading files.
-         */
-        afterAuth: null // No action.
-    };
-
-
-    function initializeModel(model) {
-        var string = model.createString('Hello Realtime World!');
-        model.getRoot().set('text', string);
-    }
-
-    var realtimeLoader = new rtclient.RealtimeLoader(realtimeOptions);
-
-    var SCOPE_ = 'https://www.googleapis.com/drive/v2/';
-
-    this.lastResponse = null;
-
-    this.__defineGetter__('SCOPE', function() {
-        return SCOPE_;
-    });
-
-    this.__defineGetter__('DOCLIST_FEED', function() {
-        return SCOPE_ + 'files';
-    });
-
-    this.__defineGetter__('CREATE_SESSION_URI', function() {
-        return 'https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable';
-    });
-
-    this.__defineGetter__('DEFAULT_CHUNK_SIZE', function() {
-        return 1024 * 1024 * 5; // 5MB;
-    });
+  this.__defineGetter__('DEFAULT_CHUNK_SIZE', function() {
+    return 1024 * 1024 * 5; // 5MB;
+  });
+  
 };
-
-function onFileLoaded(doc) {
-  console.log('file loaded');
-
-    var string = doc.getModel().getRoot().get('text');
-
-    // Keeping one box updated with a String binder.
-    var textArea1 = document.getElementById('editor1');
-    gapi.drive.realtime.databinding.bindString(string, textArea1);
-
-    // Keeping one box updated with a custom EventListener.
-    var textArea2 = document.getElementById('editor2');
-    var updateTextArea2 = function(e) {
-        textArea2.value = string;
-    };
-    string.addEventListener(gapi.drive.realtime.EventType.TEXT_INSERTED, updateTextArea2);
-    string.addEventListener(gapi.drive.realtime.EventType.TEXT_DELETED, updateTextArea2);
-    textArea2.onkeyup = function() {
-        string.setText(textArea2.value);
-    };
-    updateTextArea2();
-
-    // Enabling UI Elements.
-    textArea1.disabled = false;
-    textArea2.disabled = false;
-
-    // Add logic for undo button.
-    var model = doc.getModel();
-    var undoButton = document.getElementById('undoButton');
-    var redoButton = document.getElementById('redoButton');
-
-    undoButton.onclick = function(e) {
-        model.undo();
-    };
-    redoButton.onclick = function(e) {
-        model.redo();
-    };
-
-    // Add event handler for UndoRedoStateChanged events.
-    var onUndoRedoStateChanged = function(e) {
-        undoButton.disabled = !e.canUndo;
-        redoButton.disabled = !e.canRedo;
-    };
-    model.addEventListener(gapi.drive.realtime.EventType.UNDO_REDO_STATE_CHANGED, onUndoRedoStateChanged);
-}
 
 GDocs.prototype.auth = function(interactive, opt_callback) {
     try {
